19b49ca611982f1751cce635bdc5fb1e21f393c2
Rok Andrée <rok@andree.si>
2014-02-01 21:49:38 +0100

    Added script to compile the data into a single file and act as an interface for the API data input script

---
+++ compile.php
@@ -0,0 +1,44 @@
+<?php
+
+$dataFiles = scandir('data/');
+$logoFiles = scandir('logo/');
+$flagFiles = scandir('country-flag/');
+
+$output = array(
+	'data' => array(),
+	'logo' => array(),
+	'flag' => array()
+);
+
+foreach ($dataFiles as $filename) {
+	if (preg_match('/.+\.json$/i', $filename) === 1) {
+		$ppdata = file_get_contents('data/' . $filename);
+		$ppdata = json_decode($ppdata);
+
+		$output['data'][$ppdata->partyCode] = $ppdata;
+	}
+}
+
+foreach ($logoFiles as $filename) {
+	if (preg_match('/(.+)\.(png|jpg)$/i', $filename, $matches) === 1) {
+		$output['logo'][strtoupper($matches[1])] = 'logo/'.$filename;
+	}
+}
+foreach ($flagFiles as $filename) {
+	if (preg_match('/(.+)\.(png|jpg)$/i', $filename, $matches) === 1) {
+		$output['flag'][strtoupper($matches[1])] = 'country-flag/'.$filename;
+	}
+}
+
+//
+// This is used to more easily parse the data when importing into the API
+// 
+// API import script will include this file, with the defined constant. This way
+// the data is returned into a PHP variable, not requiring the creation of the file.
+// 
+if(defined('INCLUDE_RETURN_DATA')) {
+	return $output;
+} else {
+    file_put_contents('compiled_data.json', json_encode($output, JSON_PRETTY_PRINT));
+}
+
